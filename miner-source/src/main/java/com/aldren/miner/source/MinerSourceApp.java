fe4776eca880891095af89e50a6c6211b18b35e3
iamaldren <aldrenbobiles@gmail.com>
2021-08-13 19:45:35 +0800

    miner source

---
+++ miner-source/src/main/java/com/aldren/miner/source/MinerSourceApp.java
@@ -0,0 +1,13 @@
+package com.aldren.miner.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinerSourceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MinerSourceApp.class, args);
+    }
+}
---
+++ miner-source/src/test/java/com/aldren/miner/source/MinerSourceAppTest.java
@@ -0,0 +1,20 @@
+package com.aldren.miner.source;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MinerSourceAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
