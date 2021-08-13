ce809dd951b7decce6dee326b57bfba798e1ab71
iamaldren <aldrenbobiles@gmail.com>
2021-08-13 20:00:04 +0800

    twitter configuration class

---
+++ miner-source/src/main/java/com/aldren/miner/source/config/TwitterConfig.java
@@ -0,0 +1,36 @@
+package com.aldren.miner.source.config;
+
+import com.aldren.miner.source.properties.MinerSourceProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.social.UserIdSource;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
+
+@Configuration
+public class TwitterConfig implements SocialConfigurer {
+
+    private MinerSourceProperties sourceProps;
+
+    public TwitterConfig(MinerSourceProperties sourceProps) {
+        this.sourceProps = sourceProps;
+    }
+
+    @Override
+    public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
+        connectionFactoryConfigurer.addConnectionFactory(new TwitterConnectionFactory(sourceProps.getConsumer().getKey(),sourceProps.getConsumer().getSecret()));
+    }
+
+    @Override
+    public UserIdSource getUserIdSource() {
+        return null;
+    }
+
+    @Override
+    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+        return null;
+    }
+}
