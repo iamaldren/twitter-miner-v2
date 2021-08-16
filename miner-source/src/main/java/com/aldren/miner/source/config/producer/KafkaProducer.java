531284b3a289771ee6fa1edc9786b755cd618559
iamaldren <aldrenbobiles@gmail.com>
2021-08-16 17:44:52 +0800

    updated producer code

--- miner-source/src/main/java/com/aldren/miner/source/config/producer/KafkaProducer.java
+++ miner-source/src/main/java/com/aldren/miner/source/config/producer/KafkaProducer.java
@@ -3,13 +3,18 @@ package com.aldren.miner.source.config.producer;
 import com.aldren.miner.source.properties.MinerSourceProperties;
 import com.aldren.miner.source.service.TwitterService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.social.NotAuthorizedException;
 import org.springframework.social.twitter.api.Tweet;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
+@Slf4j
 @Configuration
 @AllArgsConstructor
 public class KafkaProducer {
@@ -19,7 +24,30 @@ public class KafkaProducer {
 
     @Bean
     public Supplier<List<Tweet>> tweets() {
-        return () -> twitterService.mineTweets(minerSourceProperties.getKeyword());
+        AtomicBoolean isFirstRun = new AtomicBoolean();
+        isFirstRun.set(true);
+
+        return () -> {
+            if(!isFirstRun.get()) {
+                try {
+                    Thread.sleep(30000L);
+                } catch (InterruptedException e) {
+                    log.error(e.getLocalizedMessage(), e);
+                }
+            }
+
+            List<Tweet> tweets = new ArrayList<>();
+            try {
+                tweets = twitterService.mineTweets(minerSourceProperties.getKeyword());
+            } catch (NotAuthorizedException e) {
+                log.warn(e.getLocalizedMessage(), e);
+            }
+
+            if(isFirstRun.get()) {
+                isFirstRun.set(false);
+            }
+            return tweets;
+        };
     }
 
 }
