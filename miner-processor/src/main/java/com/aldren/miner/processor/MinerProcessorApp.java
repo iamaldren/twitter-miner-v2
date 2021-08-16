76f2ef49fd607df93478b3760c91e4b44bcb2b65
Aldren <aldrenbobiles@gmail.com>
2021-08-17 00:01:38 +0800

    miner-processor app

---
+++ miner-processor/src/main/java/com/aldren/miner/processor/MinerProcessorApp.java
@@ -0,0 +1,13 @@
+package com.aldren.miner.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinerProcessorApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MinerProcessorApp.class, args);
+    }
+}
